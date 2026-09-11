54a7a492a41f7e0f58871334bd529a24c41dbd4b
Nafi Adhdhukha <nafiadhdhukha@gmail.com>
2026-09-12 04:08:32 +0700

    feat: add generic node appearance hook

--- core/scene/node.js
+++ core/scene/node.js
@@ -1,12 +1,18 @@
 export class Node {
-  constructor({ id, x = 0, y = 0, z = 0, opacity = 1 } = {}) {
+  constructor({ id, x = 0, y = 0, z = 0, opacity = 1, appearance = {} } = {}) {
     if (!id) throw new Error('Node requires an id');
     this.id = id;
     this.transform = { x, y, z, scaleX: 1, scaleY: 1, rotation: 0 };
     this.opacity = opacity;
+    this.appearance = { ...appearance };
   }
 
   snapshot() {
-    return JSON.parse(JSON.stringify({ id: this.id, transform: this.transform, opacity: this.opacity }));
+    return JSON.parse(JSON.stringify({
+      id: this.id,
+      transform: this.transform,
+      opacity: this.opacity,
+      appearance: this.appearance,
+    }));
   }
 }
